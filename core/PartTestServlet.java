4e1d976fd9dbe2cf0fb1926c641c37778fa0b58b
heejung-choi <choi951008@naver.com>
2020-01-21 17:44:14 +0900

    chagnes

--- core/PartTestServlet.java
+++ core/PartTestServlet.java
@@ -9,23 +9,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 @WebServlet("/part")
-@MultipartConfig  
+@MultipartConfig
 public class PartTestServlet extends HttpServlet {   
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	request.setCharacterEncoding("utf-8");
         Collection<Part> parts = request.getParts();
+        //getParts라는 메소드 사용 @MultipartConfig 이것이 정의되어 있지 않으면 에러
+		/* 파트객체만큼 request */
         System.out.println("========== 요청 받음 =========");
-        for(Part part : parts) {        	
+        for(Part part : parts) {    //파츠객체들을 하나씩 꺼내서    	
             System.out.print("name : ");
-            System.out.println(part.getName());            
+            System.out.println(part.getName());   //input 태크에서 name속성이 있다. getName()로 꺼내준다.         
             System.out.println("[ 헤더 정보 ] ");
-            for(String headerName : part.getHeaderNames()) {
+            for(String headerName : part.getHeaderNames()) {//getHeaderNames()네임 꺼내줘
                 System.out.print(headerName + " : ");
-                System.out.println(part.getHeader(headerName));
+                System.out.println(part.getHeader(headerName));              
             }
-            System.out.println("size : "+ part.getSize());
-            String filename = part.getSubmittedFileName();
-            if (filename != null)
+            System.out.println("size : "+ part.getSize());//실제 content의 사이즈
+            //업로드된 파일이 아닌 경우에는 입력되는 데이터 사이즈
+            String filename = part.getSubmittedFileName(); //실제 업로드 된 파일의 이름
+            if (filename != null) //null이 아니면 출력
             	System.out.println("file name : "+filename);
             System.out.println("------------------------------------");
         }        
--- core/UploadServlet.java
+++ core/UploadServlet.java
@@ -11,7 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 @WebServlet("/upload")
-@MultipartConfig (location = "c:/uploadtest")
+@MultipartConfig (location = "c:/uploadtest") //여기 저장하라는 설정 이것 안하고 아래 절대패스 줘도 된다.
+/* @MultipartConfig : 서블릿 3.0부터 구현 가능 */
 public class UploadServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
@@ -24,14 +25,15 @@ public class UploadServlet extends HttpServlet {
 		}
 		Collection<Part> parts = request.getParts();
 		for (Part part : parts) {
-			if (part.getContentType() != null) {
-				String fileName = part.getSubmittedFileName();
+			if (part.getContentType() != null) {//업로드 되는 파일 일 경우(null이라면 사용자가 입력한 데이터)
+				//해당 컨텐츠에 업로드 되는 파일이 있느냐?
+				String fileName = part.getSubmittedFileName();//업로드되는 파일의 이름을 꺼내는 메소드
 				if (fileName != null) {
-					part.write(fileName.substring(0, fileName.lastIndexOf(".")) + 
+					part.write(fileName.substring(0, fileName.lastIndexOf(".")) + //part 객체가 제공하는 write를 사용하면 된다.
 							"_"	+ System.currentTimeMillis() 
 							+ fileName.substring(fileName.lastIndexOf(".")));
 					out.print("<br>업로드한 파일 이름: " + fileName);
-					out.print("<br>크기: " + part.getSize());				
+					out.print("<br>크기: " + part.getSize());		//정해진 크기보다 큰사이즈라면 알려줘야 한다.		
 				}
 			} else {
 				String partName = part.getName();
